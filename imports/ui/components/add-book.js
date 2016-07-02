771dcac5c077dee2471cf8d6428b8c9ed41b45e0
Michael Hobbs <michael.lee.hobbs@gmail.com>
2016-07-02 12:08:03 -0400

    first commit

--- imports/ui/components/add-book.js
+++ imports/ui/components/add-book.js
@@ -1,32 +1,32 @@
 import React from 'react';
 import { FormGroup, FormControl } from 'react-bootstrap';
 import { Bert } from 'meteor/themeteorchef:bert';
-import { insertDocument } from '../../api/documents/methods.js';
+import { insertBook } from '../../api/books/methods.js';
 
-const handleInsertDocument = (event) => {
+const handleInsertBook = (event) => {
   const target = event.target;
   const title = target.value.trim();
 
   if (title !== '' && event.keyCode === 13) {
-    insertDocument.call({
+    insertBook.call({
       title,
     }, (error) => {
       if (error) {
         Bert.alert(error.reason, 'danger');
       } else {
         target.value = '';
-        Bert.alert('Document added!', 'success');
+        Bert.alert('Book added!', 'success');
       }
     });
   }
 };
 
-export const AddDocument = () => (
+export const AddBook = () => (
   <FormGroup>
     <FormControl
       type="text"
-      onKeyUp={ handleInsertDocument }
-      placeholder="Type a document title and press enter..."
+      onKeyUp={ handleInsertBook }
+      placeholder="Type a book title and press enter..."
     />
   </FormGroup>
 );
