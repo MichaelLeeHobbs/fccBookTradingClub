a4d59f88823b49e8e0def11e56449c1165622bd6
Michael Hobbs <michael.lee.hobbs@gmail.com>
2016-07-10 07:35:03 -0400

    Renamed Documents to Books and added method to validate user is Logged in and/or is owner of a book they are trying to modify/delete

--- imports/api/books/methods.js
+++ imports/api/books/methods.js
@@ -1,44 +1,87 @@
-import { Documents } from './documents';
+import { Books } from './books';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import { rateLimit } from '../../modules/rate-limit.js';
+import { Bert } from 'meteor/themeteorchef:bert';
 
-export const insertDocument = new ValidatedMethod({
-  name: 'documents.insert',
+function mustBeLoggedIn(schema, method, action) {
+  if (!Meteor.userId()) {
+    // Throw errors with a specific error code
+    throw new Meteor.Error(`${schema}s.methods.${method}.notLoggedIn`,
+      `Must be logged in to '${action} ${schema.toLowerCase()}'.`);
+  }
+}
+
+function mustBeOwner(schema, method, action, id) {
+  let book = Books.findOne({_id: id});
+  if (!book) {
+    // todo improve this
+    throw new Meteor.Error(`${schema}s.methods.${method}.notOwner`, `Book not found.`);
+  }
+
+  if (Meteor.userId() !== book.owner) {
+    // Throw errors with a specific error code
+    throw new Meteor.Error(`${schema}s.methods.${method}.notOwner`,
+      `Must be the owner to '${action} ${schema.toLowerCase()}'.`);
+  }
+}
+
+function mustBeLoggedInAndOwner(schema, method, action, id) {
+  mustBeLoggedIn(schema, method, action);
+  mustBeOwner(schema, method, action, id);
+}
+
+export const insertBook = new ValidatedMethod({
+  name: 'books.insert',
   validate: new SimpleSchema({
-    title: { type: String },
+    title: {type: String},
   }).validator(),
-  run(document) {
-    Documents.insert(document);
+  run(book) {
+    mustBeLoggedIn('Book', 'insert', 'add a');
+    book.owner = Meteor.userId();
+    Books.insert(book);
   },
 });
 
-export const updateDocument = new ValidatedMethod({
-  name: 'documents.update',
+export const searchBook = new ValidatedMethod({
+  name: 'books.search',
   validate: new SimpleSchema({
-    _id: { type: String },
-    'update.title': { type: String, optional: true },
+    title: {type: String},
+  }).validator(),
+  run(book) {
+    mustBeLoggedIn('Book', 'search', 'search');
+  },
+});
+
+
+export const updateBook = new ValidatedMethod({
+  name: 'books.update',
+  validate: new SimpleSchema({
+    _id: {type: String},
+    'update.title': {type: String, optional: true},
   }).validator(),
   run({ _id, update }) {
-    Documents.update(_id, { $set: update });
+    mustBeLoggedInAndOwner('Book', 'update', 'update a', _id);
+    Books.update(_id, {$set: update});
   },
 });
 
-export const removeDocument = new ValidatedMethod({
-  name: 'documents.remove',
+export const removeBook = new ValidatedMethod({
+  name: 'books.remove',
   validate: new SimpleSchema({
-    _id: { type: String },
+    _id: {type: String},
   }).validator(),
   run({ _id }) {
-    Documents.remove(_id);
+    mustBeLoggedInAndOwner('Book', 'remove', 'remove this', _id);
+    Books.remove(_id);
   },
 });
 
 rateLimit({
   methods: [
-    insertDocument,
-    updateDocument,
-    removeDocument,
+    insertBook,
+    updateBook,
+    removeBook,
   ],
   limit: 5,
   timeRange: 1000,
