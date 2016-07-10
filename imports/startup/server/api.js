4f8df99e7082a1c1f4e5510e2d3bba50dbc4b4a0
Michael Hobbs <michael.lee.hobbs@gmail.com>
2016-07-10 07:27:33 -0400

    added api load for bookSearch

--- imports/startup/server/api.js
+++ imports/startup/server/api.js
@@ -1,2 +1,3 @@
-import '../../api/documents/methods.js';
-import '../../api/documents/server/publications.js';
+import '../../api/books/methods.js';
+import '../../api/books/server/publications.js';
+import '../../api/bookSearch/server/publications.js';
