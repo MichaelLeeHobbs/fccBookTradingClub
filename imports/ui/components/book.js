4b1cd0eca5c47a0df89b2b39b873405d74cad647
Michael Hobbs <michael.lee.hobbs@gmail.com>
2016-07-10 07:40:53 -0400

    Renamed to book. Modified buttons to be reactive. If user is owner then they will see a button to remove the book. If not owner then they will see a button to request trade.

--- imports/ui/components/book.js
+++ imports/ui/components/book.js
@@ -1,61 +1,77 @@
 import React from 'react';
 import { Row, Col, ListGroupItem, FormControl, Button } from 'react-bootstrap';
 import { Bert } from 'meteor/themeteorchef:bert';
-import { updateDocument, removeDocument } from '../../api/books/methods.js';
+import { updateBook, removeBook } from '../../api/books/methods.js';
 
-const handleUpdateDocument = (documentId, event) => {
+const handleUpdateBook = (bookId, event) => {
   const title = event.target.value.trim();
   if (title !== '' && event.keyCode === 13) {
-    updateDocument.call({
-      _id: documentId,
-      update: { title },
+    updateBook.call({
+      _id: bookId,
+      update: {title},
     }, (error) => {
       if (error) {
         Bert.alert(error.reason, 'danger');
       } else {
-        Bert.alert('Document updated!', 'success');
+        Bert.alert('Book updated!', 'success');
       }
     });
   }
 };
 
-const handleRemoveDocument = (documentId, event) => {
+const handleRemoveBook = (bookId, event) => {
   event.preventDefault();
   // this should be replaced with a styled solution so for now we will
   // disable the eslint `no-alert`
   // eslint-disable-next-line no-alert
   if (confirm('Are you sure? This is permanent.')) {
-    removeDocument.call({
-      _id: documentId,
+    removeBook.call({
+      _id: bookId,
     }, (error) => {
       if (error) {
         Bert.alert(error.reason, 'danger');
       } else {
-        Bert.alert('Document removed!', 'success');
+        Bert.alert('Book removed!', 'success');
       }
     });
   }
 };
 
-export const Document = ({ document }) => (
-  <ListGroupItem key={ document._id }>
-    <Row>
+export const Book = ({ book }) => (
+  <Row>
+    <ListGroupItem key={ book._id }>
+      <Col sm={ 4 }>
+        <img src={ book.coverUrl }/>
+      </Col>
+
       <Col xs={ 8 } sm={ 10 }>
         <FormControl
           type="text"
           standalone
-          defaultValue={ document.title }
-          onKeyUp={ handleUpdateDocument.bind(this, document._id) }
+          defaultValue={ book.title }
+          onKeyUp={ handleUpdateBook.bind(this, book._id) }
         />
       </Col>
-      <Col xs={ 4 } sm={ 2 }>
-        <Button
-          bsStyle="danger"
-          className="btn-block"
-          onClick={ handleRemoveDocument.bind(this, document._id) }>
-          Remove
-        </Button>
-      </Col>
-    </Row>
-  </ListGroupItem>
+      { (Meteor.userId() === book.owner) ?
+        <Col xs={ 4 } sm={ 2 }>
+          <Button
+            bsStyle="danger"
+            className="btn-block"
+            onClick={ handleRemoveBook.bind(this, book._id) }>
+            Remove
+          </Button>
+        </Col>
+        :
+        <Col xs={ 4 } sm={ 2 }>
+          <Button
+            bsStyle="info"
+            className="btn-block"
+            onClick={ handleRemoveBook.bind(this, book._id) }>
+            Request Trade
+          </Button>
+        </Col>
+      }
+
+    </ListGroupItem>
+  </Row>
 );
