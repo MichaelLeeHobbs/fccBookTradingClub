bd642291b3f049f5db1781077f4fc7df8b7ad061
Michael Hobbs <michael.lee.hobbs@gmail.com>
2016-07-10 07:30:11 -0400

    added functionality to handle book search

--- imports/ui/components/add-book.js
+++ imports/ui/components/add-book.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { FormGroup, FormControl } from 'react-bootstrap';
 import { Bert } from 'meteor/themeteorchef:bert';
 import { insertBook } from '../../api/books/methods.js';
+import { searchBook } from '../../api/books/methods.js';
 
 const handleInsertBook = (event) => {
   const target = event.target;
@@ -21,11 +22,25 @@ const handleInsertBook = (event) => {
   }
 };
 
-export const AddBook = () => (
+const handleSearchBook = (event, props) => {
+  const target = event.target;
+  const title = target.value.trim();
+
+  if (title !== '' && event.keyCode === 13) {
+    console.log('handleSearchBook ', this);
+    props.onUserInput(title);
+
+  }
+};
+
+export const AddBook = (props) => (
   <FormGroup>
     <FormControl
       type="text"
-      onKeyUp={ handleInsertBook }
+      onKeyUp={ (event) =>{
+        console.log('props', props)
+        handleSearchBook(event, props);
+      } }//handleSearchBook }
       placeholder="Type a book title and press enter..."
     />
   </FormGroup>
