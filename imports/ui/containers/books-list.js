63c7c146e0f296fb805c0ce2db03a2ae41f60467
Michael Hobbs <michael.lee.hobbs@gmail.com>
2016-07-10 07:26:26 -0400

    experimental - testing different ways to display books are search results in the same component

--- imports/ui/containers/books-list.js
+++ imports/ui/containers/books-list.js
@@ -1,14 +1,35 @@
 import { composeWithTracker } from 'react-komposer';
 import { Books } from '../../api/books/books.js';
+import { BookSearch } from '../../api/bookSearch/bookSearch.js';
 import { BooksList } from '../components/books-list.js';
 import { Loading } from '../components/loading.js';
 import { Meteor } from 'meteor/meteor';
 
 const composer = (params, onData) => {
+  console.log(`params.subscription: ${params.subscription}  params.subscriptionParam: ${params.subscriptionParam}`);
+
   const subscription = Meteor.subscribe('books');
   if (subscription.ready()) {
-    const books = Books.find().fetch();
-    onData(null, { books });
+    let books;
+    if (params.id) {
+      books = Books.find({owner: params.id}).fetch();
+    } else {
+      books = Books.find().fetch();
+    }
+    if (! params.search) {
+      console.log('books: ', {books});
+      onData(null, {books});
+    }
+  }
+
+  let search;
+  if (params.search) {
+    search = Meteor.subscribe('bookSearch', params.subscriptionParam);
+    if(search.ready()){
+      let searchResults = BookSearch.find().fetch();
+      console.log('search: ', searchResults.length, searchResults);
+      onData(null, { books: searchResults });
+    }
   }
 };
 
