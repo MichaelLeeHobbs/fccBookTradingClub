0e5ca5c13ac722595506b0d43656d068fd24e638
Michael Hobbs <michael.lee.hobbs@gmail.com>
2016-07-10 07:27:07 -0400

    renamed document to book and added routes for add books and my books

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Meteor } from 'meteor/meteor';
 import { App } from '../../ui/layouts/app';
-import { Documents } from '../../ui/pages/documents';
+import { Books } from '../../ui/pages/books';
 import { Index } from '../../ui/pages/index';
 import { Login } from '../../ui/pages/login';
 import { NotFound } from '../../ui/pages/not-found';
@@ -20,12 +20,21 @@ const requireAuth = (nextState, replace) => {
   }
 };
 
+const onEnterAddBook = (nextState, replace) => {
+  requireAuth(nextState, replace);
+  //Meteor.call('initBookSearch');
+};
+
 Meteor.startup(() => {
+  // <IndexRoute name="index" component={ Index } onEnter={ requireAuth } />
+
   render(
     <Router history={ browserHistory }>
       <Route path="/" component={ App }>
-        <IndexRoute name="index" component={ Index } onEnter={ requireAuth } />
-        <Route name="documents" path="/documents" component={ Documents } onEnter={ requireAuth } />
+        <IndexRoute name="index" component={ Index } />
+        <Route name="books" path="/books" component={ Books } onEnter={ requireAuth } />
+        <Route name="add-book" path="/addbook" component={ Books } onEnter={ onEnterAddBook } />
+        <Route name="books" path="/books/:id" component={ Books } onEnter={ requireAuth } />
         <Route name="login" path="/login" component={ Login } />
         <Route name="recover-password" path="/recover-password" component={ RecoverPassword } />
         <Route name="reset-password" path="/reset-password/:token" component={ ResetPassword } />
