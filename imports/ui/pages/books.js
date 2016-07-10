5cd6471bef3f4296969e7571ca8b8ba912445966
Michael Hobbs <michael.lee.hobbs@gmail.com>
2016-07-10 07:29:04 -0400

    changed const Books into class. Added functionality to validateSearch and handleUserInput

--- imports/ui/pages/books.js
+++ imports/ui/pages/books.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
-import DocumentsList from '../containers/documents-list.js';
-import { AddDocument } from '../components/add-document.js';
+import BooksList from '../containers/books-list.js';
+import { AddBook } from '../components/add-book.js';
 
-export const Documents = () => (
-  <Row>
-    <Col xs={ 12 }>
-      <h4 className="page-header">Documents</h4>
-      <AddDocument />
-      <DocumentsList />
-    </Col>
-  </Row>
-);
+export class Books extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subscription: 'books',
+      subscriptionParam: ''
+    };
+  }
+
+  validateSearch(search) {
+    return (typeof search === 'string') && search.length >= 4;
+  }
+
+  handleUserInput(search) {
+    console.log(`page.books.handleUserInput.search: ${search}`)
+    this.setState({
+      search: this.validateSearch(search),
+      subscriptionParam: search
+    });
+  }
+
+//export const Books = (params) => {
+  //console.log(`pages/books.js: ${JSON.stringify(params)}`);
+  render() {
+    return (<Row>
+        <Col xs={ 12 }>
+          <h4 className="page-header">Add Book</h4>
+          <AddBook onUserInput={this.handleUserInput.bind(this)}/>
+          <BooksList id={ this.props.params.id } search={this.state.search} subscriptionParam={this.state.subscriptionParam}/>
+        </Col>
+      </Row>
+    )
+  }
+}
