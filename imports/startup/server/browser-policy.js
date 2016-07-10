c851c8981100976692372119d60c554e9ed4864c
Michael Hobbs <michael.lee.hobbs@gmail.com>
2016-07-10 07:15:28 -0400

    Allow content from local and openlibrary.org

--- imports/startup/server/browser-policy.js
+++ imports/startup/server/browser-policy.js
@@ -1,2 +1,6 @@
-// import { BrowserPolicy } from 'meteor/browser-policy-common';
+import { BrowserPolicy } from 'meteor/browser-policy-common';
 // e.g., BrowserPolicy.content.allowOriginForAll( 's3.amazonaws.com' );
+// Allow images from Open Library
+BrowserPolicy.content.allowImageOrigin( '*.openlibrary.org' );
+// Allow fonts from local
+BrowserPolicy.content.allowFontDataUrl(  );
